578bef23626ec93fb7b0dca108c34b3d65e7f143
denisbubutanu <149842477+denisbubutanu@users.noreply.github.com>
2025-12-23 18:44:53 +0000

    Update US phone number validator.js

--- Other JavaScript Projects/US phone number validator/US phone number validator.js
+++ Other JavaScript Projects/US phone number validator/US phone number validator.js
@@ -9,13 +9,17 @@ const clearButton = document.querySelector("#clear-btn");
 const checkInput = () => {
   const phoneNumber = inputPhoneNumber.value
   if (phoneNumber === "") {
+    // An alert pop-up if the input field is empty
     alert("Please provide a phone number");
   } else {
+      // A rather lengthy regex checking the many forms of US phone numbers
       const phoneRegex = /^(1)?(-|\s)?(([0-9][0-9][0-9])|(\([0-9][0-9][0-9]\)))(-|\s)?([0-9][0-9][0-9])(-|\s)?([0-9][0-9][0-9][0-9])$/;
+      // If the input matches the regex, it calls validAppend on it. Else, it calls invalidAppen
   return phoneNumber.match(phoneRegex) === null ? invalidAppend(phoneNumber) : validAppend(phoneNumber)
   }
 }
 
+// An HTML element is created whose textContent is the one below, followed by the input number. 
 const validAppend = (number) => {
   const resultChild = document.createElement("div")
   resultChild.className = "result-child"
@@ -23,7 +27,7 @@ const validAppend = (number) => {
   resultField.appendChild(resultChild)
 }
 
-
+// An HTML element is created whose textContent is the one below, followed by the input number. 
 const invalidAppend = (number) => {
   const resultChild = document.createElement("div")
   resultChild.className = "result-child"
@@ -32,13 +36,14 @@ const invalidAppend = (number) => {
 }
 
 
-
+// Clears the input field and gets rid of the HTML elements created
 const clearResults = () => {
   resultField.innerHTML = "";
   inputPhoneNumber.value = "";
 }
 
-
+/* Event listener for the check button, which checks the input
+by calling the checkInput function */
 checkButton.addEventListener("click", checkInput);
 
 inputPhoneNumber.addEventListener("keydown", (e) => {
@@ -47,4 +52,5 @@ inputPhoneNumber.addEventListener("keydown", (e) => {
   }
 });
 
+// Event listener for the clear button
 clearButton.addEventListener("click", clearResults);
